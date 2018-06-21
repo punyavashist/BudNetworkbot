55fff89eda7009188a39bf8dfef7ee19efa8b549
Punya Vashist <punyavashist@Punyas-MacBook-Pro.local>
2018-06-20 17:49:02 -0700

    Added server.pu

---
+++ server.py
@@ -0,0 +1,23 @@
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+bot_token = 618192312:AAH6y167h43ZZGdFgZ45Qcqg5vZt1l4vIh8
+
+def get_url(method):
+  return "https://api.telegram.org/bot{}/{}".format(bot_token,method)
+
+def process_message(update):
+    data = {}
+    data["chat_id"] = update["message"]["from"]["id"]
+    data["text"] = "I can hear you!"
+    r = requests.post(get_url("sendMessage"), data=data)
+
+@app.route("/{}".format(bot_token), methods=["POST"])
+def process_update():
+    if request.method == "POST":
+        update = request.get_json()
+        if "message" in update:
+            process_message(update)
+        return "ok!", 200
